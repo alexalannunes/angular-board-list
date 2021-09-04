9e27120a36384f3282ced35d3218529221ab7fe9
Alex Alan Nunes <alexalannunes@gmail.com>
2021-09-04 10:28:58 -0300

    first commit :tada:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,44 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'test-list-board';
+
+  a = 'çaex';
+
+  lists = [
+    {
+      id: 1,
+      title: 'List',
+      newItem: '',
+      items: [
+        {
+          title: 'Lotem',
+          completed: false,
+          listId: 1,
+        },
+      ],
+    },
+  ];
+
+  addList() {
+    this.lists.push({
+      id: (Math.random() * 100) | 0,
+      title: 'List',
+      newItem: '',
+      items: [],
+    });
+  }
+
+  addItemToList(item: any) {
+    item.items.push({
+      title: item.newItem,
+      completed: false,
+      listId: item.listId,
+    });
+
+    item.newItem = '';
+  }
 }
